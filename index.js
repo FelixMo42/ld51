0364e028405b547f4a800dade835044b03d721d4
Felix Moses <felix@tacocat.com>
2022-10-01 13:45:31 -0700

    60 seconds of game

--- index.js
+++ index.js
@@ -1,324 +1,247 @@
 const EDIT_MODE = getParameter("edit_mode")
-const LEVEL_ID = getParameter("level") || 0
-const SAVE_ID = getParameter("save_id")
 const RADIUS = 60
 
-function getPlayerSize(name) {
-    return RADIUS * 3/4
-}
-
-function getPlayerSpeed(player) {
-    if (player.name === "rover") return 1.5
-    return 1
-}
-
-function MouseHandler() {
-    const mouse = {
-        hex: Hex(0, 0),
-        position: { x: 0, y: 0 } 
-    }
-
-    // Create the sprite to show what hex the mouse is over
-    let graphics = new PIXI.Graphics()
-    graphics.beginFill(0xaaaaaa)
-    graphics.drawCircle(0, 0, 10)
-    game.stage.addChild(graphics)
-
-    let path = new PIXI.Graphics()
-    game.stage.addChild(path)
-
-    window.onmousemove = (e) => {
-        mouse.position = {
-            x: e.pageX,
-            y: e.pageY,
-        }
-    }
-
-    window.onmousedown = (e) => {
-        if (!e.shiftKey) {
-            setPlayerPath(player, pathfind(pixelToHex(player.sprite), mouse.hex))
-            rotatePlayers()
-        }
-
-        if (e.shiftKey && EDIT_MODE) {
-            updateHexFill(mouse.hex)
-        }
-    }
-
-    game.ticker.add(() => {
-        // Update the mouse hex
-        mouse.hex = nearestHex(pixelToHex({
-            x: mouse.position.x - game.stage.x,
-            y: mouse.position.y - game.stage.y,
-        }))
-
-        // Move the mouse sprite
-        const {x, y} = hexToPixel(mouse.hex)
-        graphics.x = x
-        graphics.y = y
-
-        // Redraw the path
-        path.clear()
-        path.lineStyle(5, 0xffffff, 0.5)
-        path.moveTo(x, y)
-        for (const node of pathfind(pixelToHex(player.sprite), mouse.hex)) {
-            let { x, y } = hexToPixel(node)
-            path.lineTo(x, y)
-        }
-    })
-
-    return mouse
-}
-
-function CameraHandler() {
-    game.stage.x = window.innerWidth / 2
-    game.stage.y = window.innerHeight / 2
-
-    window.onresize = () => {
-        game.stage.x = window.innerWidth / 2
-        game.stage.y = window.innerHeight / 2
-    }
-}
-
-function doPlayersOverlap(a, b) {
-    const distance = Math.pow(Math.abs(a.sprite.x - b.sprite.x), 2)
-                   + Math.pow(Math.abs(a.sprite.y - b.sprite.y), 2)
-
-    return distance < Math.pow(getPlayerSize(a) + getPlayerSize(b), 2)
-}
-
-let speed = 1
-
-function PlayerHandler() {
-    game.ticker.add(() => {
-        for (const npc of npcs) {
-            for (const player of players) {
-                if (doPlayersOverlap(npc, player)) {
-                    speed = 0
-                    alert("You win!")
-                }
-            }
-        }
-    })
-
-    game.ticker.add(() => {
-        const dt = speed * game.ticker.deltaMS / 1000
-
-        for (const player of players) {
-            movePlayer(player, dt)
-        }
-
-        for (const npc of npcs) {
-            if (!movePlayer(npc, dt)) {
-                let distance = 0
-                let farthest = Hex(0, 0)
-                for (const hex of getAllWalkableHexs()) {
-                    if (isWalkable(hex)) {
-                        if (getHexDistanceFromPlayers(hex) > distance) {
-                            distance = getHexDistanceFromPlayers(hex)
-                            farthest = hex
-                        }
-                    }
-                }
-
-                if (!equals(farthest, npc.hex)) {
-                    setPlayerPath(npc, pathfind2(npc.hex, farthest).slice(-2))
-                }
-            }
-        }
-    })
-}
-
-function movePlayer(player, dt) {
-    // if they have no path, then they can't move
-    // return false to indicate the player needs a new path
-    if (player.path.length === 0) {
-        return false
-    }
-
-    // incrament the distance travled counter
-    player.travled += dt * getPlayerSpeed(player)
-
-    if (player.travled >= player.distance) {
-        setPlayerPath(player, player.path)
-    } else {
-        // move it move it
-        let a = hexToPixel(player.hex)
-        let b = hexToPixel(player.path[player.path.length - 1])
-        let percent = player.travled / player.distance
-        player.sprite.x = interpolate(percent, a.x, b.x)
-        player.sprite.y = interpolate(percent, a.y, b.y)
-    }
-    
-    // return true to say that yes, the player moved this frame
-    return true
-}
-
-function setPlayerPath(player, path) {
-    // There is no path, meaning it failed. Continue doing as before.
-    if (path.length === 0) return
-
-    // set coords
-    player.hex = path.pop()
-    player.path = path
-
-    // set movement timers
-    if (player.path.length > 0) {
-        player.distance = getHexDistance(player.hex, player.path[player.path.length - 1])
-        player.travled = 0
-    }
-}
-
-function getHexDistanceFromPlayers(hex) {
-    let distance = 0
-    for (const player of players) {
-        distance += getHexDistance(player.hex, hex)
-    }
-    return distance
-}
-
-function isUserControlled(name) {
-    return name === "elephant-1" || name === "elephant-2"
-}
-
-function Player({ name, hex }) {
-    // Load player sprite
-    let sprite = new PIXI.Sprite.from(`./img/${name}.png`)
-    sprite.anchor.set(0.5)
-    sprite.width = getPlayerSize() * 2
-    sprite.height = getPlayerSize() * 2
-    let { x, y } = hexToPixel(hex)
-    sprite.x = x
-    sprite.y = y
-    game.stage.addChild(sprite)
-    
-    const player = {
-        hex,
-        name,
-
-        // 
-        sprite,
-        path: [],
-        
-        // used for smooth player movment
-        distance: 0,
-        travled: 0
-    }
-
-    if (isUserControlled(name)) {
-        players.push(player)
-    } else {
-        npcs.push(player)
-    }
-}
-
-// Create the canvas and runtime, and add it to the document.
+// create the app, and it too the canvase, but don't make it visible
 const game = new PIXI.Application({ resizeTo: window })
 document.body.appendChild(game.view)
+// game.stage.renderable = false
 
-// Draw hexs over filled in hexs
-const filledHexs = new Map()
-function updateHexFill(hex) {
-    if (setWalkable(hex)) {
-        const g = new PIXI.Graphics()
-        g.beginFill(0xffffff, 0.2)
-        g.drawPolygon(getHexPoints(hex))
-        g.endFill()
-        game.stage.addChild(g)
-        filledHexs.set(getHexId(hex), g)
-    } else {
-        game.stage.removeChild(filledHexs.get(getHexId(hex)))
-        filledHexs.delete(getHexId(hex))
+// start the game
+const mouse = MouseHandler()
+const camera = CameraHandler()
+PlayerHandler()    
+
+function level1() {
+    setStage({
+        path: rowOfHexs(10),
+        players: [
+            {
+                name: "elephant-1",
+                hex: Hex(0, 0)
+            }
+        ],
+        cameraMode: "follow",
+    })
+
+    return () => {}
+}
+
+function level2() {
+    setStage({
+        path: [Hex(0, 0),Hex(-1, -1),Hex(0, 1),Hex(-1, 1),Hex(-2, 1),Hex(-3, 2),Hex(1, -2),Hex(0, 2),Hex(1, 2),Hex(-2, -1),Hex(-3, -1),Hex(-4, 0),Hex(-4, 1),Hex(2, -2),Hex(2, -1),Hex(2, 0),Hex(2, 1),Hex(-1, 3),Hex(-2, 3),Hex(-1, -2),Hex(0, -3),Hex(1, -4),Hex(2, -4),Hex(3, -4),Hex(4, -3),Hex(4, -4),Hex(3, -2),Hex(5, -3),Hex(5, -2),Hex(5, -1),Hex(4, 0),Hex(3, 1),Hex(0, -1),Hex(-5, 3),Hex(-6, 3),Hex(-5, 2),Hex(-4, 2),Hex(-5, 4),Hex(-4, 4),Hex(-3, 4)],
+        players: [
+            {
+                name: "elephant-1",
+                hex: Hex(0, 0)
+            },
+            {
+                name: "elephant-2",
+                hex: Hex(0, 1)
+            },
+            {
+                name: "rover",
+                hex: Hex(-6, 3)
+            }
+        ],
+        cameraMode: "static"
+    })
+
+    return () => {}
+}
+
+function level3() {
+    setStage({
+        path: [Hex(0, 0),Hex(-1, 1),Hex(1, 0),Hex(-2, 1),Hex(-3, 2),Hex(-4, 3),Hex(-1, 2),Hex(-1, 3),Hex(1, 1),Hex(1, 2),Hex(-2, 0),Hex(-3, 0),Hex(-4, 1),Hex(-5, 2),Hex(-5, 3),Hex(-2, -1),Hex(-1, -2),Hex(0, -2),Hex(1, -3),Hex(2, -3),Hex(2, -2),Hex(2, -1),Hex(-1, 4),Hex(2, 2),Hex(3, 1),Hex(4, 0),Hex(4, -1),Hex(4, -2),Hex(3, -2),Hex(-4, 4),Hex(-3, 4),Hex(-2, 4),Hex(-6, 2),Hex(-7, 3),Hex(-7, 4),Hex(-6, 4),Hex(-6, 5)],
+        players: [
+            {
+                name: "elephant-1",
+                hex: Hex(0, 0)
+            },
+            {
+                name: "elephant-2",
+                hex: Hex(-4, 3)
+            },
+        ],
+        cameraMode: "follow"
+    })
+
+    const fireSprite = new PIXI.Graphics()
+    fireSprite.beginFill(0xff0000, 0.5)
+    fireSprite.drawRect(
+        0,
+        -game.stage.y - window.innerHeight / 2 - 300,
+        window.innerWidth,
+        window.innerHeight
+    )
+    fireSprite.endFill()
+    game.stage.addChild(fireSprite)
+
+    function updateFire() {
+        const dt = game.ticker.deltaMS / 1000
+
+        fireSprite.x = -game.stage.x
+        fireSprite.y += dt * 50;
+    }
+
+    game.ticker.add(updateFire)
+
+    return () => {
+        game.ticker.remove(updateFire)
+        game.stage.removeChild(fireSprite)
     }
 }
 
-function rotatePlayers() {
-    selectPlayer((selectedPlayer + 1) % players.length)
+function level4() {
+    setStage({
+        path: [Hex(0, 0),Hex(-1, -1),Hex(0, 1),Hex(-1, 1),Hex(-2, 1),Hex(-3, 2),Hex(1, -2),Hex(0, 2),Hex(1, 2),Hex(-2, -1),Hex(-3, -1),Hex(-4, 0),Hex(-4, 1),Hex(2, -2),Hex(2, -1),Hex(2, 0),Hex(2, 1),Hex(-1, 3),Hex(-2, 3),Hex(-1, -2),Hex(0, -3),Hex(1, -4),Hex(2, -4),Hex(3, -4),Hex(4, -3),Hex(4, -4),Hex(3, -2),Hex(5, -3),Hex(5, -2),Hex(5, -1),Hex(4, 0),Hex(3, 1),Hex(0, -1),Hex(-5, 3),Hex(-6, 3),Hex(-5, 2),Hex(-4, 2),Hex(-5, 4),Hex(-4, 4),Hex(-3, 4)],
+        players: [
+            {
+                name: "elephant-1",
+                hex: Hex(0, 0)
+            },
+            {
+                name: "elephant-2",
+                hex: Hex(0, 1)
+            },
+            {
+                name: "pelican",
+                hex: Hex(-6, 3)
+            },
+            {
+                name: "pelican",
+                hex: Hex(3, -4)
+            },
+            {
+                name: "pelican",
+                hex: Hex(3, -4)
+            },
+            {
+                name: "pelican",
+                hex: Hex(-2, -1)
+            }
+        ],
+        cameraMode: "static"
+    })
+
+    return () => {}
 }
 
-function selectPlayer(id) {
-    if (player) {
-        player.sprite.tint = 0xffffff 
-    }
+function level5() {
+    setStage({
+        path: [Hex(0, 0),Hex(-1, -1),Hex(0, 1),Hex(-1, 1),Hex(-2, 1),Hex(-3, 2),Hex(1, -2),Hex(0, 2),Hex(1, 2),Hex(-2, -1),Hex(-3, -1),Hex(-4, 0),Hex(-4, 1),Hex(2, -2),Hex(2, -1),Hex(2, 0),Hex(2, 1),Hex(-1, 3),Hex(-2, 3),Hex(-1, -2),Hex(0, -3),Hex(1, -4),Hex(2, -4),Hex(3, -4),Hex(4, -3),Hex(4, -4),Hex(3, -2),Hex(5, -3),Hex(5, -2),Hex(5, -1),Hex(4, 0),Hex(3, 1),Hex(0, -1),Hex(-5, 3),Hex(-6, 3),Hex(-5, 2),Hex(-4, 2),Hex(-5, 4),Hex(-4, 4),Hex(-3, 4)],
+        players: [
+            {
+                name: "elephant-1",
+                hex: Hex(0, 0)
+            },
+            {
+                name: "elephant-2",
+                hex: Hex(0, 1)
+            },
+            {
+                name: "police",
+                hex: Hex(-6, 3)
+            },
+            {
+                name: "police",
+                hex: Hex(3, -4)
+            },
+            {
+                name: "police",
+                hex: Hex(3, -4)
+            },
+            {
+                name: "police",
+                hex: Hex(-2, -1)
+            }
+        ],
+        cameraMode: "static"
+    })
 
-    selectedPlayer = id
-    player = players[selectedPlayer]
-
-    player.sprite.tint = 0x4682b4
+    return () => {}
 }
 
-// list of all players in the scene
-const players = []
-const npcs = []
+function level6() {
+    let hexs = new Set()
+    function getRandomHex() {
+        let hex = Hex(
+            random(-2, 2),
+            random(2, 5),
+        )
+        if (hexs.has(getHexId(hex))) {
+            return getRandomHex()
+        } else {
+            hexs.add(getHexId(hex))
+            return hex
+        }
+    }
 
-function loadLevel() {
-    if (SAVE_ID) {
-        return JSON.parse(localStorage.getItem(SAVE_ID))
-    } else {
-        return fetch(`./lvl/${LEVEL_ID}.json`).then(res => res.json())
+    setStage({
+        path: hexCirlce(2, 10),
+        players: [
+            {
+                name: "elephant-1",
+                hex: Hex(-1, 1)
+            },
+            {
+                name: "elephant-2",
+                hex: Hex(1, 0)
+            },
+            ...Array(10).fill(0).map((_, i) => ({
+                name: `protestor-${i + 1}`,
+                hex: getRandomHex()
+            }))
+        ],
+        cameraMode: "follow"
+    })
+
+    return () => {}
+}
+
+function credits() {
+    game.stage.renderable = false
+    return () => {}
+}
+
+async function run() {
+    const levels = [
+        level1,
+        level2,
+        level3,
+        level4,
+        level5,
+        level6,
+        credits,
+    ]
+
+    if (levels.length === 1) return levels[0]()
+
+    for (const level of levels) {
+        const destructor = level()
+        await sleep(10)
+        destructor()
     }
 }
 
-async function init() {
-    // retrive level data
-    const level = await loadLevel()
+function sleep(seconds) {
+    return new Promise((resolve) => setTimeout(resolve, seconds * 1000))
+}
 
-    // add walkable paths
-    for (const hex of level.path) {
-        updateHexFill(hex)
+run()
+
+if (false) {
+    window.onmousedown = () => {
+        updateHexFill(mouse.hex)
+        console.log(mouse.hex)
     }
+}
 
-    // add players
-    for (const player of level.players) {
-        Player(player)
+function saveHexs() {
+    for (const hex of walkableGraphics.keys()) {
+
     }
-    
-    // select the first one
-    selectPlayer(0)
-
-    // start the game
-    start()
 }
 
 async function save() {
-    let text = JSON.stringify({
-        path: getAllWalkableHexs(),
-        players: players.map((player) => ({
-            name: player.name,
-            hex: player.hex
-        }))
-    })
-    
-    const name = await prompt("save name")
-    localStorage.setItem(name, text)
-    navigator.clipboard.writeText(text)
-}
-
-async function load() {
-
-}
-
-function start() {
-    MouseHandler()
-    CameraHandler()
-    PlayerHandler()    
-}
-
-init()
-
-// Selected player
-let selectedPlayer = -1;
-let player = null
-
-if (EDIT_MODE) {
-    document.onkeyup = async (e) => {
-        if (e.key === "s") {
-            save()
-        } else if (e.key === "l") {
-            let name = prompt("save name")
-            let url = new URL(window.location.href)
-            url.searchParams.set('save_id', name)
-            window.location.href = url
-        }
-    }
+    navigator.clipboard.writeText(JSON.stringify(getAllWalkableHexs()))
 }
---
+++ notes.md
@@ -0,0 +1,38 @@
+# levels
+
+There are 6 ten second levels.
+Each of them will have a one second opening slide.
+
+They fallow a pair of twins.
+Each new scean is 5 years latter (going from 5-30).
+
+## 1 (age 5): elephant island
+
+- one twin goes down a hallway.
+- map tries to get them to go off coars around
+- goal: reach twin
+
+## 2 (age 10): there is nothing to fear, grow up
+
+- twins chase rover on a trash island
+- goal: catch rover
+
+## 3 (age 15): the world burns
+
+- twins flee from a fire the desends down the screen
+- goal: reach boats
+
+## 4 (age 20): work shall set you free
+
+- twins try to catch chikens
+- goal: catch all the chikens
+
+## 5 (age 25): police
+
+- twins run awayfrom the police
+- goal: don't get caught
+
+## 6 (age 30): a surging crowd lifts all boats
+
+- a sweeping crowd follows the twins
+- goal: none
